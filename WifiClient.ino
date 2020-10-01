b41f1a18ce4c9359de0b07e57122097c576184ec
Teran326 <swysog@gmail.com>
2020-10-01 16:35:23 +0200

    1.10.

---
+++ WifiClient.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>      // Include the Wi-Fi library
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid     = "wifi";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "0700848237";     // The password of the Wi-Fi network
+ESP8266WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());    // Send the IP address of the ESP8266 to the computer
+  server.begin();
+  server.on("/", [](){
+    server.send(200, "text/plain", "Hello world!");
+    });
+}
+
+void loop() {
+  server.handleClient();
+}
